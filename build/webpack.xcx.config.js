15186177070a7bba954136245e89c5417008ef5e
yc <kixi@163.com>
2018-10-29 16:36:09 +0800

    小程序的splittrunk优化为2

--- build/webpack.xcx.config.js
+++ build/webpack.xcx.config.js
@@ -181,7 +181,7 @@ function baseConfig(asset, envAttributs) {
             chunks: 'all',
             name: 'common', // 任意命名
             // minSize: 30000, // 只要超出0字节就生成一个新包
-            minChunks: 3, // 只要超出0字节就生成一个新包
+            minChunks: 2, // 只要超出0字节就生成一个新包
             priority: 10
           }
         }
