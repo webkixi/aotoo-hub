5e6e5837add44838702b48e01279a6b64e6771b2
lgh <826293349@qq.com>
2019-05-31 15:00:22 +0800

    完善tab组件

--- build/lib/xcx/components/tab/index.js
+++ build/lib/xcx/components/tab/index.js
@@ -7,6 +7,7 @@ const app = getApp()
 const Core = require('../aotoo/core')
 const lib = Core.lib
 
+
 function grabData(dataSource) {
   let ds = dataSource
   ds.select = ds.select ? ds.select : 0
@@ -23,31 +24,42 @@ function grabData(dataSource) {
           title: item.title,
           tap: 'tabIndex?idx='+ii,
           itemClass: ds.select == ii ? 'active' : ''
-        })
-        _contents.push(item.content)
+        }) 
+        //id: 'tabs-'这个目前必须是固定，下面的获取节点需要啊啊啊
+        _contents.push(
+          typeof item.content == 'string'
+          ? {
+              title: item.content,
+              // attr: {id: 'tabs-'+ii},
+              id: 'tabs-'+ii
+            }
+          : item.content
+        )
       }
     })
   }
-
+  // let toView
   const menus = {
     type: _isScroll ? {
       is: 'scroll',
       'scroll-x': true,
-      'scroll-y': false
+      'scroll-y': false,
+      'scroll-with-animation': true
     } : '',
     data: _menus,
     listClass: 'menu-box',
     itemClass: 'menu-item'
   }
-
   const contents = {
     $$id: '__mytabcontent',
     data: _contents,
     type: ds.multipy ? 
       {
         is: 'scroll',
-        'scroll-x': false,
-        'scroll-y': true
+        'scroll-y': true,
+        'scroll-into-view': '',
+        bindscroll: 'tabScroll',
+        // 'scroll-with-animation': true
       }  
     : {
       is: 'swiper',
@@ -110,7 +122,11 @@ Component({
   behaviors: [Core.baseBehavior(app, 'tap')],
   lifetimes: {
     created: function () {
-      this.savePrevSelect = []
+      this.savePrevSelect = []      //用来判断导航条下边线的的动画
+      this.query = wx.createSelectorQuery().in(this)      //获取节点需要的
+      this.queryMenus = wx.createSelectorQuery().in(this)      //获取节点需要的
+      this.getValue = []        //存储组件内节点的信息
+      this.getMenusValue = []        //存储组件内节点的信息
       this.generateUpdate('$dataSource', function() {
         const {menus, contents} = grabData($dataSource)
         this.setData({
@@ -132,33 +148,98 @@ Component({
     ready: function() {
       const ds = this.data.$dataSource
       this.mount((ds.$$id || ds.id))
+      let $contentQuery = []
+      let $menusQuery = []
+      for(let i = 0; i < ds.data.length; i++) {
+        this.query.select('.tab-container >>> #tabs-'+i).boundingClientRect()
+      }
+      this.queryMenus.selectAll('.tab-container >>> .menu-item').boundingClientRect()
+      this.queryMenus.selectViewport().scrollOffset(function(res){
+        res.scrollTop // 显示区域的竖直滚动位置
+      })
+      this.query.exec(function (res) {
+        $contentQuery = res
+      })
+      this.queryMenus.exec(function(res){
+        $menusQuery = res[0]
+      })
+      setTimeout(() => {
+        this.getValue = $contentQuery
+        this.getMenusValue = $menusQuery
+      }, 100);
     },
   },
   methods: {
+    isSelect: false,      //点击导航栏时 不触发滚动时会触发的menusFunc
+    menusFunc: function(data, idx) {
+      this.savePrevSelect.length > 1 ? this.savePrevSelect = this.savePrevSelect.slice(1) : this.savePrevSelect
+      return data.map((item, ii) => {
+        if (item.itemClass) item.itemClass = ''
+        if (ii == idx) {
+          this.savePrevSelect.push(idx)
+          item.itemClass = this.savePrevSelect.length > 1 ? this.savePrevSelect[0] > this.savePrevSelect[1] ? 'f-right active' : 'active' : 'active'
+        }
+        return item
+      })
+    },
+    menusScroll: function(idx){
+      e.detail.x + this.getMenusValue[idx].width > wx.getSystemInfoSync().screenWidth
+    },
     tabIndex: function(e, param) {
       if (param) {
+        this.isSelect = true
         const idx = parseInt(param['idx'])
         const $menus = this.data.$menus
-        this.savePrevSelect.length > 1 ? this.savePrevSelect = this.savePrevSelect.slice(1) : this.savePrevSelect
-
-        $menus.data = $menus.data.map((item, ii) => {
-          if (item.itemClass) item.itemClass = ''
-          if (ii==idx) {
-            this.savePrevSelect.push(idx)
-            item.itemClass = this.savePrevSelect.length > 1 ? this.savePrevSelect[0] > this.savePrevSelect[1] ? 'f-right active' : 'active' : 'active'
-          }
-          return item
-        })
-
-        this.setData({ 
+        $menus.data = this.menusFunc($menus.data, idx)
+        //e.detail.x < this.getMenusValue[idx].width  代表点击的地方是左边
+        //e.detail.x + this.getMenusValue[idx].width > wx.getSystemInfoSync().screenWidth 代表点击的区域超过屏幕
+        this.data.$dataSource.multipy
+        ? e.detail.x + this.getMenusValue[idx].width > wx.getSystemInfoSync().screenWidth || e.detail.x < this.getMenusValue[idx].width
+          ?
+          this.setData({ 
+              $menus,
+              '$menus.type.scroll-left': e.detail.x < this.getMenusValue[idx].width ? e.currentTarget.offsetLeft - 30 :  this.getMenusValue[idx].right - wx.getSystemInfoSync().screenWidth + this.getMenusValue[idx].width,
+              '$contents.type.scroll-top': this.getValue[idx].top - this.getValue[0].top,   //导航条高（44）+ padding（15） = 59
+              '$contents.type.bindscroll': 'tabScroll?content-'+idx,
+            })
+          :
+          this.setData({ 
+            $menus,
+            '$contents.type.scroll-top': this.getValue[idx].top - this.getValue[0].top,   //导航条高（44）+ padding（15） = 59
+            '$contents.type.bindscroll': 'tabScroll?content-'+idx,
+          })
+        : 
+        this.setData({
           $menus,
-          '$contents.type.current': idx
+          '$contents.type.current': idx,
         })
       }
     },
-    bindscroll: function(e, param) {
-    },
-    bindscrolltolower: function(e, param) {},
-    bindscrolltoupper: function(e, param) {},
+    tabScroll: function(e, param) {
+      if (this.isSelect == false) {
+        this.getValue.map( (item, ii) => {
+          //原理是 a > 0 && a < b, 滚动的值要大于前一个top，且小于当前top, 只为把当前的值 进行menus 滚动
+          if ((ii > 0 ? e.detail.scrollTop > (this.getValue[ii - 1].top - this.getValue[0].top - 10) : '') && e.detail.scrollTop <= (item.top - this.getValue[0].top) ){
+            const $idx = item.id.substr(item.id.length - 1, 1) - 1
+            const $menus = this.data.$menus
+            $menus.data = this.menusFunc($menus.data, $idx)
+            this.getMenusValue[$idx].right > wx.getSystemInfoSync().screenWidth
+            ?
+              this.setData({ 
+                $menus,
+                '$menus.type.scroll-left': this.getMenusValue[$idx].right - wx.getSystemInfoSync().screenWidth + this.getMenusValue[$idx].width,
+              })
+            :
+              this.setData({ 
+                $menus,
+                '$menus.type.scroll-left': 0      //这个地方有待优化，现在的做法是向上滚的时候，就把scroll-left： 0
+              })
+          }
+        })
+      }
+      else {
+        this.isSelect = false
+      }
+    }
   }
 })
