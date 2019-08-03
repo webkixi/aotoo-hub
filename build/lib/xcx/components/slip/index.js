58691efd83b51e2567f9d64c5fb2621ec97e943d
yc <kixi@163.com>
2019-08-04 03:01:13 +0800

    组件slip优化

--- build/lib/xcx/components/slip/index.js
+++ build/lib/xcx/components/slip/index.js
@@ -7,20 +7,6 @@ const app = getApp()
 const Core = require('../aotoo/core')
 const lib = Core.lib
 
-let tpos = {
-  instance: undefined,
-  start: { x: 0, y: 0 },
-  current: undefined,
-  distance: 0,
-  events: {},
-  moving: false,
-  timeStamp: {
-    gap: 500,
-    start: 0,
-    end: 0
-  }
-}
-
 function slipParse(dataSource={}) {
   if (lib.isArray(dataSource.data)){
     dataSource.data = dataSource.data.map(item=>{
@@ -29,10 +15,14 @@ function slipParse(dataSource={}) {
       }
       if (lib.isObject(item)) {
         item.id = lib.suid('slip_')
-        item.catchtouchstart = item.catchtouchstart || item.touchstart || true
-        item.catchtouchmove = item.catchtouchmove || item.touchmove || true
-        item.catchtouchend = item.catchtouchend || item.touchend || true
-        item.catchtouchcancel = item.catchtouchcancel || item.touchcancel || true
+        item.catchtouchstart = 'tstart'
+        item.catchtouchmove = 'tmove'
+        item.catchtouchend = 'tend'
+        item.catchtouchcancel = 'tcancel'
+        // item.catchtouchstart = item.catchtouchstart || item.touchstart || true
+        // item.catchtouchmove = item.catchtouchmove || item.touchmove || true
+        // item.catchtouchend = item.catchtouchend || item.touchend || true
+        // item.catchtouchcancel = item.catchtouchcancel || item.touchcancel || true
         if (lib.isArray(item.li)) {
           item.li = item.li.map(itm=>{
             if (itm.tap || itm.fake_tap) {
@@ -66,7 +56,7 @@ function slipParse(dataSource={}) {
       option: dataSource.option || (dataSource.type && dataSource.type.option) || ['a', 10, 60]
     }
 
-    dataSource.type.option.length = dataSource.data.length
+    dataSource.type.option[4] = dataSource.data.length
   }
   return dataSource
 }
@@ -114,7 +104,11 @@ Component({
   behaviors: [Core.listBehavior(app, 'slip')],
   lifetimes: {
     created: function() {
+      this.startTimmer = ''
       this.hooks.on('update', function(param) {
+        if (lib.isArray(param)) {
+          param = { data: param }
+        }
         if (lib.isObject(param)) {
           Object.keys(param).forEach(key=>{
             if (key.indexOf('data') != -1) {
@@ -154,14 +148,13 @@ Component({
       const $type = $list.type
       const $data = $list.data
       let _slip = $type.option
-      let slip = {
+      this.slipOption = {
         direction: _slip[0] || slipObj.direction,
         min: _slip[1] || slipObj.min,
         width: _slip[2] || slipObj.width,
         find: _slip[3] || slipObj.find,
         length: $data.length || 0
       }
-      tpos.slip = slip
     }
   },
   methods: {
@@ -172,62 +165,78 @@ Component({
       return this
     },
     tstart(e, param, inst) {
-      const changeIt = e.touches[0] || e.changedTouches[0]
-      const target = e.currentTarget
-      const id = target.id
-      const pageX = changeIt.pageX
-      const pageY = changeIt.pageY
-
-      const query = this.createSelectorQuery()
-      const myinst = query.select(`#${id}`)
-      myinst.boundingClientRect()
-      query.exec(function (res) {
-        const _target = res[0]
-        if (tpos.instance) {
-          if (tpos.instance.id == _target.id) {
-
-          } else {
-            // 还原 tpos.instance.id 实例的初始状态
-          }
-          tpos.instance = Object.assign({}, tpos.instance, {
-            top: _target.top,
-            left: _target.left,
-            right: _target.right,
-            bottom: _target.bottom
-          })
-        } else {
-          tpos.instance = {
-            id: target.id,
-            treeid: target.dateset.treeid,
-            top: _target.top,
-            left: _target.left,
-            right: _target.right,
-            bottom: _target.bottom,
-            width: _target.width,
-            height: _target.height
-          }
-        }
-      })
-
-      tpos = {
-        start: {
-          x: pageX,
-          y: pageY
-        },
-        timeStamp: {
-          start: e.timeStamp
-        },
-        instance: {
-          id: target.id,
-          // treeid: target.dateset.treeid
-        }
-      }
+      clearTimeout(this.startTimmer)
+      this.startTimmer = setTimeout(() => {
+        this.findAndUpdate(e, function(param) {
+          param.itemClass = param.itemClass + ' active'
+          return param
+        })
+      }, 700);
+      this.hooks.emit('touchstart', e)
     },
-    tmove() {
-      // console.log('=========== move');
+    tmove(e, param, inst) {
+      this.hooks.emit('touchmove', e)
     },
     tend(e, param, inst) {
-      // console.log('=========== end');
-    }
+      clearTimeout(this.startTimmer)
+      this.findAndUpdate(e, function (param) {
+        param.itemClass = param.itemClass.replace(' active', '').replace('active', '')
+        return param
+      })
+      
+      // let that = this
+      // let _data = this.getData().data
+      // let $data = lib.clone(_data)
+
+      // let currentTarget = e.currentTarget
+      // let dataset = currentTarget.dataset
+      // let slipDone = dataset.slipDone
+      // let {index, offset} = slipDone
+      // index = parseInt(index)
+      // offset = parseInt(offset)
+      // if (offset > 0) {
+      //   $data.splice((offset+1), 0, _data[index])
+      //   $data.splice(index, 1)
+      // } else {
+      //   $data.splice((index + offset), 0, _data[index])
+      //   $data.splice((index+1), 1)
+      // }
+
+      // setTimeout(() => {
+      //   that.update($data)
+      // }, 200);
+
+      this.hooks.emit('touchend', e)
+    },
   }
 })
+
+/***************************************
+ * 生成从minNum到maxNum的随机数。
+ * 如果指定decimalNum个数，则生成指定小数位数的随机数
+ * 如果不指定任何参数，则生成0-1之间的随机数。
+ *
+ * @minNum：[数据类型是Integer]生成的随机数的最小值（minNum和maxNum可以调换位置）
+ * @maxNum：[数据类型是Integer]生成的随机数的最大值
+ * @decimalNum：[数据类型是Integer]如果生成的是带有小数的随机数，则指定随机数的小数点后的位数
+ *
+ ****************************************/
+function randomNum(maxNum, minNum, decimalNum) {
+  var max = 0,
+    min = 0;
+  minNum <= maxNum ? (min = minNum, max = maxNum) : (min = maxNum, max = minNum);
+  switch (arguments.length) {
+    case 1:
+      return Math.floor(Math.random() * (max + 1));
+      break;
+    case 2:
+      return Math.floor(Math.random() * (max - min + 1) + min);
+      break;
+    case 3:
+      return (Math.random() * (max - min) + min).toFixed(decimalNum);
+      break;
+    default:
+      return Math.random();
+      break;
+  }
+}
